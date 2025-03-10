d3a3636b0b27851d6bd05eb6b2535d28ac44eef1
xfz347 <52524808+xfz347@users.noreply.github.com>
2025-03-10 20:30:20 +0800

    Update convert.js

--- pages/api/convert.js
+++ pages/api/convert.js
@@ -17,7 +17,7 @@ module.exports = async (req, res) => {
       url,
       headers: {
         "User-Agent":
-          "ClashX Pro/1.72.0.4 (com.west2online.ClashXPro; build:1.72.0.4; macOS 12.0.1) Alamofire/5.4.4",
+          "clash.meta/1.18.7",
       },
     });
     configFile = result.data;
